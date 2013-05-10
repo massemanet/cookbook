4b7118133eced21f360e4db1636a452ffb34d035
mats cronqvist <masse@klarna.com>
2013-05-10 20:47:47 +0200

    buncher server

---
+++ buncher.erl
@@ -0,0 +1,60 @@
+%% -*- mode: erlang; erlang-indent-level: 2 -*-
+%%% Created : 10 May 2013 by mats cronqvist <masse@klarna.com>
+
+%% @doc
+%% @end
+
+-module('buncher').
+-author('mats cronqvist').
+-export([start/0]).
+
+start() -> start([]).
+
+start(Opts) ->
+  case whereis(?MODULE) of
+    undefined -> register(?MODULE,spawn(fun init/0));
+    _         -> ok
+  end,
+  ?MODULE ! {go,add_defaults(Opts)},
+  whereis(?MODULE).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% options
+default_opts() ->
+  [].
+
+add_defaults(Opts) ->
+  [{K,proplists:get_value(K,Opts,V)} || {K,V} <- default_opts()].
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% server
+init() ->
+  receive
+    {go,[]} ->
+      mloop(acc_new())
+  end.
+
+mloop(Acc) ->
+  receive
+    {attach,Pid} -> mloop(acc_append(attaches,Pid,Acc));
+    {item,Val}   -> mloop(acc_append(vals,Val,Acc));
+    time         -> send(Acc),set_time(),mloop(acc_new())
+  end.
+
+send(Acc) ->
+  case acc_get(vals,Acc) of
+    [] -> ok;
+    Vs -> lists:foreach(fun(P) -> P ! Vs end, acc_get(attaches,Acc))
+  end.
+
+set_time() ->
+  erlang:send_after(1000,self(),time).
+
+acc_new() ->
+  orddict:from_list([{attaches,[]},{vals,[]}]).
+
+acc_get(K,A) ->
+  orddict:fetch(K,A).
+
+acc_append(K,V,A) ->
+  orddict:append(K,V,A).
