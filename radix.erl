ff9c6c2164187edda8662f20b48acbf776f81d82
mats cronqvist <masse@klarna.com>
2013-08-13 10:39:31 +0200

    radix.erl

---
+++ radix.erl
@@ -0,0 +1,25 @@
+%% -*- mode: erlang; erlang-indent-level: 2 -*-
+%%% Created : 26 Jun 2013 by mats cronqvist <masse@klarna.com>
+
+%% @doc
+%% encode positive integers in 2 =< base =< 62
+%% @end
+
+-module('radix').
+-author('mats cronqvist').
+-export([enc/2]).
+
+enc(0,_) -> "0";
+enc(Int,Base) ->
+  enc(Int,Base,trunc(math:log(Int)/math:log(Base)),[]).
+
+enc(0,_,_,R) ->
+  [anum(E) || E <- lists:reverse(R)];
+enc(I,B,N,R) ->
+  P = round(math:pow(B,N)),
+  M = I div P,
+  enc(I-M*P,B,N-1,[M|R]).
+
+anum(E) when E =< 9 -> E+$0;
+anum(E) when E =< 36 -> E+$A-10;
+anum(E) when E =< 62 -> E+$a-37.
