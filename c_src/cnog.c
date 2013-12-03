cb9de1a97dc6e94a7584eb0cd9c8961ba74872cb
mats cronqvist <masse@klarna.com>
2013-12-02 18:23:41 -0600

    added cnog. compiles, but untested.

---
+++ c_src/cnog.c
@@ -0,0 +1,190 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <dlfcn.h>
+#include <assert.h>
+#include "ei.h"
+#include "cnog.h"
+
+ei_cnode ec;                    /* the ei cnode description struct */
+static int fd;                  /* fd to remote Erlang node */
+static char* rem_regname;       /* process name on remote Erlang node */
+static void* library;           /* handle for dlopen */
+
+static void ABEND(char* reason) {
+  printf("ABEND: %s\n", reason);
+  exit(1);
+}
+
+void cnog_send(ei_x_buff *xbuf) {
+
+  if ( ei_reg_send(&ec, fd, rem_regname, xbuf->buff, xbuf->index) )
+    ABEND("bad send");
+}
+
+static void make_xbuf(ei_x_buff *xbuf) {
+  ei_x_new_with_version(xbuf);
+  cnog_wrap_ans("reply",xbuf);
+}
+
+static void send_xbuf(ei_x_buff *xbuf){
+  cnog_send(xbuf);
+  ei_x_free(xbuf);
+}
+
+static bool get_fpointer(char *func, ei_x_buff *xbuf,
+                         bool (*funcp)(int, ei_x_buff *, char *, int *)) {
+
+  char *error;
+
+  if ( ! library ) library = dlopen(NULL,0);
+  dlerror();                                    /* Clear any existing error */
+  *(void **) (&funcp) = dlsym(library, func);   /* this is where the magic...*/
+  if ( (error = dlerror()) == NULL ) return true;
+
+  fprintf(stderr, "could not find '%s', error %s\n", func, error);
+  cnog_enc_2_error(xbuf, "no_such_function");
+  ei_x_encode_atom(xbuf, func);
+  return false;
+}
+
+static bool make_reply(ei_x_buff *xbuf, char *buff, int *index) {
+  int k;
+  int arity;
+  char cmd[MAXATOMLEN+1];
+  bool (*funcp)(int, ei_x_buff *, char *, int *);
+
+  if ( ! ((arity = cnog_get_tuple(xbuf, buff, index)) > -1) ||
+       ! cnog_check_arity(xbuf,2,arity)  ||
+       ! cnog_get_arg_atom(xbuf, buff, index, cmd) ||
+       ! ((arity = cnog_get_list(xbuf, buff, index)) > -1) )
+    return false;
+
+  if ( get_fpointer(cmd, xbuf, funcp) &&
+       (*funcp)(arity, xbuf, buff, index) ) {
+    assert( ei_decode_list_header(buff,index,&k) == 0 );
+    assert( k == 0 );
+    return true;
+  }
+
+  return false;
+}
+
+static void send_replies(char *buff, int *index) {
+  ei_x_buff xbuf;
+  int arity, i;
+
+  make_xbuf(&xbuf);
+
+  if ( (arity = cnog_get_list(&xbuf, buff, index)) < 0 ) {
+    send_xbuf(&xbuf);
+    return;
+  }
+
+  for (i = 0; i < arity; i++) {
+    if ( ((i+1)%1000) == 0 ) {
+      ei_x_encode_empty_list(&xbuf);
+      send_xbuf(&xbuf);
+      make_xbuf(&xbuf);
+    }
+    ei_x_encode_list_header(&xbuf, 1);
+    if ( ! make_reply(&xbuf, buff, index) )
+      break;
+  }
+
+  ei_x_encode_empty_list(&xbuf);
+  send_xbuf(&xbuf);
+}
+
+static void reply(erlang_msg *msg, ei_x_buff *recv_x_buf) {
+  int index = 0;
+  int version;
+
+  ei_decode_version(recv_x_buf->buff, &index, &version);
+  send_replies(recv_x_buf->buff, &index);
+}
+
+static bool cnog_receive(void) {
+  erlang_msg msg;
+  ei_x_buff xbuf;
+
+  ei_x_new_with_version(&xbuf);
+  switch ( ei_xreceive_msg(fd, &msg, &xbuf) ){
+  case ERL_TICK:
+    break;                      /* ignore */
+  case ERL_MSG:
+    switch (msg.msgtype) {
+    case ERL_SEND:
+    case ERL_REG_SEND:
+      reply(&msg, &xbuf);
+      break;
+    case ERL_LINK:
+    case ERL_UNLINK:
+    case ERL_GROUP_LEADER:
+      break;                    /* ignore */
+    case ERL_EXIT:
+    case ERL_EXIT2:
+    case ERL_NODE_LINK:
+      return false;             /* die */
+    }
+    break;
+  case ERL_ERROR:
+    return false;               /* die */
+  }
+  ei_x_free(&xbuf);
+  return true;
+}
+
+static int cnog_loop(void) {
+  while (1) {
+    if ( ! cnog_receive() ) return 1;
+  }
+}
+
+#define REMNODE           argv[1]
+#define REMHOST           argv[2]
+#define REMREG            argv[3]
+#define COOKIE            argv[4]
+#define NODE_NAME         argv[5]
+#define ERL_DIST_VSN atoi(argv[6])
+
+static void cnog_start_cnode(char **argv) {
+  char rem_node_name[MAXATOMLEN] = "";  /* other node name */
+  ei_x_buff xbuf;
+  erlang_pid *self = ei_self(&ec);
+
+  strcat(rem_node_name,REMNODE);
+  strcat(rem_node_name,"@");
+  strcat(rem_node_name,REMHOST);
+  rem_regname = strdup(REMREG);
+  printf("I am %s, you are %s (%d)\n", NODE_NAME, rem_node_name, ERL_DIST_VSN);
+
+  ei_set_compat_rel(ERL_DIST_VSN); /* erlnode version of dist. protocol */
+
+  if ( ei_connect_init(&ec, NODE_NAME, COOKIE, 1) < 0 )
+    ABEND("ei_connect_init");
+
+  if ( (fd = ei_connect(&ec, rem_node_name)) < 0 )
+    ABEND("ei_connect failed.\nwrong cookie? erl-dist version mismatch?");
+
+  self->num = fd;               /* bug?? in ei_reg_send_tmo */
+
+  ei_x_new_with_version(&xbuf);
+  cnog_wrap_ans("handshake", &xbuf);
+  ei_x_encode_empty_list(&xbuf);
+  cnog_send(&xbuf);
+  ei_x_free(&xbuf);
+}
+
+int main(int argc, char **argv){
+
+  if ( argc != 7 ){
+    printf("Usage: %s node host regname cookie node_name erl_dist_vsn\n",
+            argv[0]);
+    return 1;
+  }
+
+  cnog_start_cnode(argv);
+  cnog_loop();
+  return 0;
+}
---
+++ c_src/cnog_marshal.c
@@ -0,0 +1,219 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#include "ei.h"
+#include "cnog.h"
+
+/***************************************************************************/
+/* wrap all answers in this */
+/* {{self(),reply|signal|error|handshake},...} */
+/***************************************************************************/
+
+void cnog_wrap_ans(char* tag, ei_x_buff* xbuf) {
+  extern ei_cnode ec;
+  ei_x_encode_tuple_header(xbuf,2);
+  ei_x_encode_tuple_header(xbuf,2);
+  ei_x_encode_pid(xbuf, ei_self(&ec));
+  ei_x_encode_atom(xbuf, tag);
+}
+
+void cnog_wrap_reply(char* tag, ei_x_buff* xbuf) {
+  ei_x_encode_tuple_header(xbuf,2);
+  ei_x_encode_atom(xbuf, tag);
+}
+/***************************************************************************/
+/* error tuple builders */
+/***************************************************************************/
+/* {{Node,error}, {Err,...}} */
+void cnog_enc_2_error(ei_x_buff *xbuf, char *err) {
+  cnog_wrap_reply("error", xbuf);
+  ei_x_encode_tuple_header(xbuf,2);
+  ei_x_encode_atom(xbuf, err);
+}
+
+/* {{Node,error}, Err} */
+void cnog_enc_1_error(ei_x_buff *xbuf, char *err) {
+  cnog_wrap_reply("error", xbuf);
+  ei_x_encode_atom(xbuf, err);
+}
+
+/***************************************************************************/
+/* marshal return values from C calls */
+/* {{Node,reply}, ...} */
+/***************************************************************************/
+void cnog_put_tuple(ei_x_buff *xbuf, int N) {
+  cnog_wrap_reply("ok", xbuf);
+  assert( ! ei_x_encode_tuple_header(xbuf,N));
+}
+
+void cnog_put_void(ei_x_buff *xbuf) {
+  cnog_wrap_reply("ok", xbuf);
+  assert( ! ei_x_encode_atom(xbuf, "void") );
+}
+
+void cnog_put_pid(ei_x_buff *xbuf, erlang_pid *p) {
+  cnog_wrap_reply("ok", xbuf);
+  assert( ! ei_x_encode_pid(xbuf, p) );
+}
+
+void cnog_put_boolean(ei_x_buff *xbuf, int b) {
+  cnog_wrap_reply("ok", xbuf);
+  assert( ! ei_x_encode_boolean(xbuf, b) );
+}
+
+void cnog_put_double(ei_x_buff *xbuf, double d) {
+  cnog_wrap_reply("ok", xbuf);
+  assert( ! ei_x_encode_double(xbuf, d) );
+}
+
+void cnog_put_longlong(ei_x_buff *xbuf, long long l) {
+  cnog_wrap_reply("ok", xbuf);
+  assert( ! ei_x_encode_longlong(xbuf, l) );
+}
+
+void cnog_put_ulonglong(ei_x_buff *xbuf, unsigned long long l) {
+  cnog_wrap_reply("ok", xbuf);
+  assert( ! ei_x_encode_ulonglong(xbuf, l) );
+}
+
+void cnog_put_atom(ei_x_buff *xbuf, char *p) {
+  cnog_wrap_reply("ok", xbuf);
+  assert( ! ei_x_encode_atom(xbuf, p) );
+}
+
+void cnog_put_string(ei_x_buff *xbuf, char *p) {
+  cnog_wrap_reply("ok", xbuf);
+  if ( p == NULL ) {
+    ei_x_encode_string(xbuf, "");
+  }else{
+    ei_x_encode_string(xbuf, p);
+  }
+}
+
+/***************************************************************************/
+/* marshalling erlang -> C */
+/***************************************************************************/
+bool cnog_get_arg_pid(ei_x_buff *xbuf, char *B, int *I, erlang_pid *pid){
+  if ( ! ei_decode_pid(B ,I, pid) ) return true;
+  cnog_enc_1_error(xbuf, "bad_pid");
+  return false;
+}
+
+bool cnog_get_arg_boolean(ei_x_buff *xbuf, char *B, int *I, int *b) {
+  if ( ! ei_decode_boolean(B, I, b) ) return true;
+  cnog_enc_1_error(xbuf, "bad_boolean");
+  return false;
+}
+
+bool cnog_get_arg_atom(ei_x_buff *xbuf, char *B, int *I, char *a){
+  if ( ! ei_decode_atom(B, I, a) ) return true;
+  cnog_enc_1_error(xbuf, "bad_atom");
+  return false;
+}
+
+/* if cnog_get_arg_string returns true, *a must be freed */
+bool cnog_get_arg_string(ei_x_buff *xbuf, char *B, int *I, char **a){
+  int type, size;
+
+  if ( ei_get_type(B, I, &type, &size) ) {
+    cnog_enc_1_error(xbuf, "bad_type");
+    return false;
+  }
+
+  *a = (char *)malloc(size+1);
+
+  if ( ! ei_decode_string(B, I, *a) ) return true;
+
+  free(*a);
+  cnog_enc_1_error(xbuf, "bad_string");
+  return false;
+}
+
+bool cnog_get_arg_short(ei_x_buff *xbuf, char *B, int *I, short *a) {
+  long long ll;
+
+  if ( ! cnog_get_arg_longlong(xbuf, B, I, &ll) ) return false;
+  *a = ll;
+  return true;
+}
+bool cnog_get_arg_int(ei_x_buff *xbuf, char *B, int *I, int *a) {
+  long long ll;
+
+  if ( ! cnog_get_arg_longlong(xbuf, B, I, &ll) ) return false;
+  *a = ll;
+  return true;
+}
+bool cnog_get_arg_long(ei_x_buff *xbuf, char *B, int *I, long *a) {
+  long long ll;
+
+  if ( ! cnog_get_arg_longlong(xbuf, B, I, &ll) ) return false;
+  *a = ll;
+  return true;
+}
+bool cnog_get_arg_longlong(ei_x_buff *xbuf, char *B, int *I, long long *a) {
+
+  if ( ! ei_decode_longlong(B ,I, a) ) return true;
+  cnog_enc_1_error(xbuf, "bad_integer");
+  return false;
+}
+
+bool cnog_get_arg_ushort(ei_x_buff *xbuf, char *B, int *I, unsigned short *a) {
+  unsigned long long ull;
+  if ( ! cnog_get_arg_ulonglong(xbuf,B,I,&ull) ) return false;
+  *a = ull;
+  return true;
+}
+bool cnog_get_arg_uint(ei_x_buff *xbuf, char *B, int *I, unsigned int *a) {
+  unsigned long long ull;
+  if ( ! cnog_get_arg_ulonglong(xbuf,B,I,&ull) ) return false;
+  *a = ull;
+  return true;
+}
+bool cnog_get_arg_ulong(ei_x_buff *xbuf, char *B, int *I, unsigned long *a) {
+  unsigned long long ull;
+  if ( ! cnog_get_arg_ulonglong(xbuf,B,I,&ull) ) return false;
+  *a = ull;
+  return true;
+}
+bool cnog_get_arg_ulonglong(ei_x_buff *xbuf, char *B, int *I,
+                            unsigned long long *ull) {
+  if ( ! ei_decode_ulonglong(B, I, ull) ) return true;
+  cnog_enc_1_error(xbuf, "bad_unsigned_integer");
+  return false;
+}
+
+bool cnog_get_arg_float(ei_x_buff *xbuf, char *B, int *I, float *a) {
+  double dbl;
+  if ( ! cnog_get_arg_double(xbuf, B, I, &dbl) ) return false;
+  *a = dbl;
+  return true;
+}
+bool cnog_get_arg_double(ei_x_buff *xbuf, char *B, int *I, double *a) {
+  if ( ! ei_decode_double(B, I, a) ) return true;
+  cnog_enc_1_error(xbuf, "bad_double");
+  return false;
+}
+
+bool cnog_check_arity(ei_x_buff *xbuf, int a1, int a2) {
+  if ( a1 == a2 ) return true;
+
+  cnog_enc_2_error(xbuf, "bad_arity");
+  ei_x_encode_long(xbuf, (long) a2);
+  return false;
+}
+
+/***************************************************************************/
+int cnog_get_list(ei_x_buff *xbuf, char *B, int *I) {
+  int ari;
+  if ( ! ei_decode_list_header(B, I, &ari) ) return ari;
+  cnog_enc_1_error(xbuf, "bad_list");
+  return -1;
+}
+
+int cnog_get_tuple(ei_x_buff *xbuf, char *B, int *I) {
+  int ari;
+  if ( ! ei_decode_tuple_header(B, I, &ari) ) return ari;
+  cnog_enc_1_error(xbuf, "bad_tuple");
+  return -1;
+}
