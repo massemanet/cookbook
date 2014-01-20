99d29a8aaf44ef608ecb6fa7e34a2d19ef60e3b3
mats cronqvist <masse@klarna.com>
2014-01-20 19:27:03 +0100

    introduce better error reporting in cnode

--- c_src/cnode.c
+++ c_src/cnode.c
@@ -16,8 +16,10 @@ int foo(int);
 int bar(int);
 
 int main(int argc, char **argv) {
+  struct sockaddr_in addr;                 /* our address */
+  int on = 1;
   int port;                                /* Listen port number */
-  int listen;                              /* Listen socket */
+  int listen_fd;                           /* Listen socket */
   int fd;                                  /* fd to Erlang node */
   ErlConnect conn;                         /* Connection data */
 
@@ -37,23 +39,46 @@ int main(int argc, char **argv) {
 
   erl_init(NULL, 0);
 
-  if (erl_connect_init(3, "secretcookie", 0) == -1) {
+  if (erl_connect_init(3, "secretcookie", 0) < 0) {
     erl_err_quit("erl_connect_init");
   }
 
-  /* Make a listen socket */
-  if ((listen = my_listen(port)) <= 0) {
-    erl_err_quit("my_listen");
+  if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket");
+    return 0;
+  }
+
+  if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
+    perror("setsockopt");
+    close(listen_fd);
+    return 0;
+  }
+
+  memset((void*) &addr, 0, (size_t) sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if (bind(listen_fd, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+    perror("bind");
+    close(listen_fd);
+    return 0;
+  }
+
+  if (listen(listen_fd, 5) < 0) {
+    perror("listen");
+    close(listen_fd);
+    return 0;
   }
 
   if (erl_publish(port) == -1) {
-    close(listen);
+    close(listen_fd);
     erl_err_quit("erl_publish");
   }
   fprintf(stderr, "published c3\n\r");
 
-  if ((fd = erl_accept(listen, &conn)) == ERL_ERROR) {
-    close(listen);
+  if ((fd = erl_accept(listen_fd, &conn)) == ERL_ERROR) {
+    close(listen_fd);
     erl_err_quit("erl_accept");
   }
   fprintf(stderr, "Connected to %s\n\r", conn.nodename);
@@ -64,10 +89,9 @@ int main(int argc, char **argv) {
       /* ignore */
     } else if (got == ERL_ERROR) {
       close(fd);
-      close(listen);
+      close(listen_fd);
       return 0;
     } else {
-
       if (emsg.type == ERL_REG_SEND) {
         fromp = erl_element(2, emsg.msg);
         tuplep = erl_element(3, emsg.msg);
@@ -90,32 +114,6 @@ int main(int argc, char **argv) {
       }
     }
   }
-  close(fd);
-  close(listen);
-  return 0;
-}
-
-
-int my_listen(int port) {
-  int listen_fd;
-  struct sockaddr_in addr;
-  int on = 1;
-
-  if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    return (-1);
-
-  setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-
-  memset((void*) &addr, 0, (size_t) sizeof(addr));
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(port);
-  addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-  if (bind(listen_fd, (struct sockaddr*) &addr, sizeof(addr)) < 0)
-    return (-1);
-
-  listen(listen_fd, 5);
-  return listen_fd;
 }
 
 int foo(int i) {
