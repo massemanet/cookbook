7d8f246244a6885709c26d9d65e5d6a4e7307670
mats cronqvist <masse@klarna.com>
2014-01-20 19:45:33 +0100

    add state display to boilerplate

--- src/boilerplate.erl
+++ src/boilerplate.erl
@@ -8,7 +8,7 @@
 -author('mats cronqvist').
 
 %% the API
--export([start/0,stop/0,unlink/0]).
+-export([start/0,stop/0,state/0,unlink/0]).
 
 %% for application supervisor
 -export([start_link/0]).
@@ -18,6 +18,16 @@
 -export([init/1,terminate/2,code_change/3,
          handle_call/3,handle_cast/2,handle_info/2]).
 
+%% declare the state
+-record(b,{uno=1,due=[1,2]}).
+-record(state,{a={},b=#b{}}).
+
+%% add all records here, to kludge around the record kludge.
+rec_info(state) -> record_info(fields,state);
+rec_info(b)     -> record_info(fields,b);
+rec_info(_)     -> [].
+
+%% the API
 start() ->
   application:start(?MODULE).
 
@@ -27,11 +37,14 @@ stop() ->
 unlink() ->
   gen_server:call(?MODULE,unlink).
 
+state() ->
+  gen_server:call(?MODULE,state).
+
+%% for application supervisor
 start_link() ->
   gen_server:start_link({local,?MODULE},?MODULE,[],[]).
 
--record(state,{}).
-
+%% gen_server callbacks
 init(_) ->
   {ok,#state{}}.
 
@@ -41,6 +54,8 @@ terminate(_Reason,_State) ->
 code_change(_OldVsn,State,_Extra) ->
   {ok,State}.
 
+handle_call(state,_From,State) ->
+  {reply,expand_recs(State),State};
 handle_call(unlink,_From,State) ->
   {links,Links} = process_info(self(),links),
   lists:foreach(fun unlink/1,Links),
@@ -53,3 +68,20 @@ handle_cast(_What,State) ->
 
 handle_info(_What,State) ->
   {noreply,State}.
+
+%% utilities
+expand_recs(List) when is_list(List) ->
+  [expand_recs(I) || I <- List];
+expand_recs(Tup) when is_tuple(Tup) ->
+  case tuple_size(Tup) of
+    L when L < 1 -> Tup;
+    L ->
+      try Fields = rec_info(element(1,Tup)),
+          L = length(Fields)+1,
+          lists:zip(Fields,expand_recs(tl(tuple_to_list(Tup))))
+      catch _:_ ->
+          list_to_tuple(expand_recs(tuple_to_list(Tup)))
+      end
+  end;
+expand_recs(Term) ->
+  Term.
