8be02e4f90ac3ea42606a6cea738c52b6a554ee3
mats cronqvist <masse@klarna.com>
2014-01-21 11:21:18 +0100

    remove macro from boilerplate

--- src/boilerplate_sup.erl
+++ src/boilerplate_sup.erl
@@ -8,9 +8,6 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
-
 %% ===================================================================
 %% API functions
 %% ===================================================================
@@ -23,4 +20,8 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, [?CHILD(boilerplate,worker)]} }.
+  {ok, {{one_for_one, 5, 10}, [child(boilerplate,worker)]}}.
+
+child(I, Type) ->
+  {I, {I, start_link, []}, permanent, 5000, Type, [I]}.
+
