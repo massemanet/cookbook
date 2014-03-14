0dee257de74e5f53e2ed560c01cfe52514ff14c8
mats cronqvist <masse@klarna.com>
2014-03-14 15:34:18 +0100

    handle radix edge case

--- src/radix.erl
+++ src/radix.erl
@@ -11,10 +11,12 @@
 
 enc(0,_) -> "0";
 enc(Int,Base) ->
-  enc(Int,Base,trunc(math:log(Int)/math:log(Base)),[]).
+  enc(Int,Base,trunc(math:log(Int+0.1)/math:log(Base)),[]).
 
-enc(0,_,_,R) ->
+enc(0,_,N,R) when N < 0 ->
   [anum(E) || E <- lists:reverse(R)];
+enc(0,B,N,R) ->
+  enc(0,B,N-1,[0|R]);
 enc(I,B,N,R) ->
   P = round(math:pow(B,N)),
   M = I div P,
