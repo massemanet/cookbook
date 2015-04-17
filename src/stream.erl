89d571896391a1d354f9b0168c10ce443c7d9811
mats cronqvist <mats.cronqvist@gmail.com>
2015-04-17 16:37:19 +0200

    streamy

--- src/stream.erl
+++ src/stream.erl
@@ -7,33 +7,35 @@
 -module('stream').
 -export([new/1,new/2,new/3,next/1,fold/3]).
 
+-record(stream,{state=nil,next,exit=nil}).
+
 new(Next)           -> new(Next,nil).
 new(Next,Init)      -> new(Next,Init,nil).
-new(Next,Init,Exit) -> {stream,maybe_init(Init),Next,Exit}.
+new(Next,Init,Exit) -> #stream{state=maybe_init(Init),next=Next,exit=Exit}.
 
-next({stream,State,Next,Exit}) ->
+next(S) ->
   try
-    {Element,Nstate} = Next(State),
-    {Element,{stream,Nstate,Next,Exit}}
+    {Element,Nstate} = (S#stream.next)(S#stream.state),
+    {Element,S#stream{state=Nstate}}
   catch
-    throw:finished -> maybe_exit(Exit)
+    throw:finished -> maybe_exit(S)
   end.
 
-fold(Fun,Acc,{stream,State,Next,Exit}) ->
-  case next({stream,State,Next,Exit}) of
-    {El,Nstream} ->
-      case nacc(El,Fun,Acc,Exit) of
-        {ok,Nacc} -> fold(Fun,Nacc,Nstream);
+fold(Fun,Acc,S) ->
+  case next(S) of
+    {El,NS} ->
+      case nacc(El,Fun,Acc,NS) of
+        {ok,Nacc} -> fold(Fun,Nacc,NS);
         finished -> Acc
       end;
     finished -> Acc
   end.
 
-nacc(El,Fun,Acc,Exit) ->
+nacc(El,Fun,Acc,S) ->
   try {ok,Fun(El,Acc)}
   catch
     C:R ->
-      maybe_exit(Exit),
+      maybe_exit(S),
       case {C,R} of
         {throw,finished} -> finished;
         {throw,_}        -> throw(R);
@@ -44,5 +46,7 @@ nacc(El,Fun,Acc,Exit) ->
 maybe_init(Init) when is_function(Init) -> Init();
 maybe_init(_) -> nil.
 
-maybe_exit(Exit) when is_function(Exit) -> Exit(),finished;
+maybe_exit(S) when is_function(S#stream.exit) -> stream_exit(S),finished;
 maybe_exit(_) -> finished.
+
+stream_exit(S) -> (S#stream.exit)(S#stream.state).
