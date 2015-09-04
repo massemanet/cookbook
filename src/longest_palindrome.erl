e58a8fdd088976ece4e56cba7355c16e75a26fba
mats cronqvist <masse@cronqvi.st>
2015-09-04 15:05:56 +0200

    works

--- src/longest_palindrome.erl
+++ src/longest_palindrome.erl
@@ -8,7 +8,19 @@
 -export([go/0,go/1]).
 
 go() ->
-  [go(X) || X <- ["abcdefgfedfrg","abcdefg"].
+  [go(X) || X <- ["abcdefgfedfrg","abcdefg"]].
 
 go(X) ->
-  [].
+  try [test(string:substr(X,S,L)) || L <- lengths(X), S <- starts(L,X)]
+  catch A -> A
+  end.
+
+lengths(X) -> lists:reverse(lists:seq(1,length(X))).
+
+starts(L,X) -> lists:seq(1,length(X)-L+1).
+
+test(X) ->
+  case X =:= lists:reverse(X) of
+    true -> throw(X);
+    false-> ok
+  end.
